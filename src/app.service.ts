370a3b6c5a3fa384f765016489e2fe12f0218076
Martin Janeček <admin@ikw.cz>
2023-01-05 15:28:36 +0000

    feat: add fastify

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { HttpAdapterHost } from '@nestjs/core';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly httpHost: HttpAdapterHost) {}
   getHello(): string {
-    return 'Hello World!';
+    return `Hello World from ${this.httpHost.httpAdapter.getType()}`;
   }
 }
